6c959f076e86f4a67b19ad24166adb3b524734e9
Bridgeshantanu <131445481+Bridgeshantanu@users.noreply.github.com>
2023-10-11 00:06:49 +0530

    assignment9 code is added

---
+++ WordRearrange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTest
+{
+    public class WordRearrange
+    {
+        public string EligibleForNextRound(string word1, string word2)        
+        {           
+            char[] charArray1 = word1.ToLower().ToCharArray();
+            char[] charArray2 = word2.ToLower().ToCharArray();
+            
+            Array.Sort(charArray1);
+            Array.Sort(charArray2);
+            
+            if (new string(charArray1) == new string(charArray2))
+            {
+                return "YES";
+            }
+            else
+            {
+                return "NO";
+            }
+
+        }
+    }
+}
